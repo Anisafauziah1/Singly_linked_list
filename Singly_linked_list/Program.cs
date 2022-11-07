b0c9b89362fddad5b9965a929f07d12390ac9e99
Anisa Fauziah <anisa.fauziah.ft21@mail.umy.ac.id>
2022-11-07 17:06:47 +0700

    add a note in the list

--- Singly_linked_list/Program.cs
+++ Singly_linked_list/Program.cs
@@ -13,8 +13,36 @@ namespace Singly_linked_list
         public string name;
         public Node next;
     }
-
-
+    class List 
+    {
+        Node START;
+        public List() 
+        {
+            START = null;
+        }
+        public void addNote() // add a note in the list
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the roll name of the student: ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+            // if the node to be inserted is the first node
+            if(START == null || rollNo <= START.rollNumber)
+            {
+                if((START != null) &&(rollNo == START.rollNumber))
+                {
+                    Console.WriteLine();
+                    return;
+                }
+                
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
